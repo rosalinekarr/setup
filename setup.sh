e7cf62ce7880764270a332f1b0af9a0f21c8d5ca
Rose Karr <rose@rosalinekarr.com>
2018-10-12 13:43:44 -0400

    Added homebrew fonts

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Add Casks to Homebrew
-brew tap caskroom/cask
-
 # Install command line tools
 brew install git
 brew install go
@@ -12,6 +9,9 @@ brew install hub
 brew install mackup
 brew install rbenv
 brew install tmux
+
+# Add Casks to Homebrew
+brew tap caskroom/cask
 
 # Install Casks
 brew cask install 1password
@@ -23,6 +23,12 @@ brew cask install keybase
 brew cask install opera
 brew cask install slack
 brew cask install zoomus
+
+# Add Fonts to Homebrew
+brew tap homebrew/cask-fonts
+
+# Install Fonts
+brew cask install font-hack
 
 # Create mackup config file
 echo "[storage]
