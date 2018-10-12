e84aeac24f3df6e275fe7ddf14a76def30b99f16
Rose Karr <rose@rosalinekarr.com>
2018-10-12 12:48:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup
+
+This is my personal setup script. I run it whenever I reformat my laptop.
+
+## To run
+
+```
+curl https://raw.githubusercontent.com/rosalinekarr/setup/master/setup.sh | sh
+```
---
+++ setup.sh
@@ -0,0 +1,30 @@
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Add Casks to Homebrew
+brew tap caskroom/cask
+
+# Install command line tools
+brew install git
+brew install go
+brew install gpg
+brew install hub
+brew install mackup
+brew install rbenv
+brew install tmux
+
+# Install Casks
+brew cask install 1password
+brew cask install atom
+brew cask install brave
+brew cask install firefox
+brew cask install google-chrome
+brew cask install keybase
+brew cask install opera
+brew cask install slack
+
+# Create mackup config file
+curl https://raw.githubusercontent.com/rosalinekarr/setup/master/.mackup.cfg > ~/.mackup.cfg
+
+# Restore preferences backup
+mackup restore
